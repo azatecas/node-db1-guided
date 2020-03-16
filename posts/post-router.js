e32d66d02573ed84376112f0be27e495c4448f93
Luis PM <luisp237@gmail.com>
2020-03-16 13:36:32 -0400

    add knex sqlite3 as dependancies     *made a get request in the POST-ROUTER.JS     *in POSTMAN able to get request to http//:localhos:{PORT}/api/posts

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require('../data/db-config.js'); //connection to the data base, we will learn this later
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    //get the data form the db
+    //select * from posts <--sql equivelant
+    db.select('*').from('posts') //returns a promise
+        .then(rows => {
+            res.status(200).json({ data: rows });
+        })
+        .catch(err => {
+            res.status(500).json({ message: "sorry, ran into an errorr" });
+        })
 
 });
 
