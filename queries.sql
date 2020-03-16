68c84ec7c2a511ad65433b03ac15b5c583fcc4ba
Luis PM <luisp237@gmail.com>
2020-03-16 12:46:30 -0400

    sql language to read data ascending/descending

--- queries.sql
+++ queries.sql
@@ -1,6 +1,32 @@
 -- Read Data
+    SELECT CustomerName, City, Country 
+    FROM Customers;
+
+-- READ Data, all suppliers from the USA only (use ' ' and db can be configure to be Case sensitive)  
+    SELECT Country, City, SupplierName 
+    FROM Suppliers
+    WHERE Country = 'USA';
+
+-- READ data from customers in USA only
+    SELECT *
+    FROM Customers
+    WHERE Country = 'USA';
+
+--READ with OR operator and ORDER BY and DESC
+    SELECT *
+    FROM Customers
+    WHERE Country = 'USA' or City = 'Paris'
+    ORDER BY Country DESC;
+
+--READ order by country ascending, then by city descending
+    SELECT *
+    FROM Customers
+    WHERE Country = 'USA' or City = 'Paris'
+    ORDER BY Country, City DESC;
+
 
 -- Add Data
+
 
 -- Update Data
 
