575f50880247744ddbcff0e93bf83c36bd3cebbc
Luis PM <luisp237@gmail.com>
2020-03-16 13:14:15 -0400

    SQLite Crud commands written on the queries.sql

--- queries.sql
+++ queries.sql
@@ -24,10 +24,25 @@
     WHERE Country = 'USA' or City = 'Paris'
     ORDER BY Country, City DESC;
 
-
+--READ 5 MOst expensive products
+    SELECT * FROM [Products]
+    WHERE Price > 25
+    order by price desc
+    LIMIT 5; 
 -- Add Data
-
+    INSERT INTO [shippers] (ShipperName, Phone)
+    VALUES('Lambda Shipping', '(555) 555-1234');
 
 -- Update Data
+--WRONG wAY
+    UPDATE [Shippers]
+    SET Phone = '(505) 555-1234'; --DANGER DANGER NO 'WHERE' CLAUSE, ALL RECORDS UPDATE IF YOU DON'T SPECIFY WITH WHERE!!!!!!!!
+
+--UPDATE CORRECT
+    --SELECT * FROM [Shippers]
+    UPDATE Shippers SET Phone = '(555) 555-5555'
+    WHERE ShipperID = 4;
 
 -- Delete Data
+    DELETE FROM Shippers
+    WHERE ShipperID = 4;
